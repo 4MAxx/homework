852f73b06a654fcc51e7e7bd99e0cf9f16da8a97
4MAxx <mad_@tut.by>
2022-05-16 19:54:16 +0300

    Классы, объекты, наследование. 2. Квадрат

--- Klasses_obj_inherit.py
+++ Klasses_obj_inherit.py
@@ -12,10 +12,20 @@ class Rectangle:
                 print('*', end='')
             print(end='\n')
 
-r1 = Rectangle()
-r2 = Rectangle(5, 3)
-print(r1.calculate_area())
-print(r2.calculate_area())
-r1.draw()
-r2.draw()
+# r1 = Rectangle()
+# r2 = Rectangle(5, 3)
+# print(r1.calculate_area())
+# print(r2.calculate_area())
+# r1.draw()
+# r2.draw()
 
+class Square(Rectangle):
+    def __init__(self, length=3):
+        super().__init__(length, length)
+
+s1 = Square()
+s1.draw()
+print(s1.calculate_area())
+s2 = Square(6)
+s2.draw()
+print(s2.calculate_area())
