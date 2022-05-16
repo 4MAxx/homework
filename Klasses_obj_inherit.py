c64f40d18570e4be7b969158f2da4f822f350711
4MAxx <mad_@tut.by>
2022-05-16 19:40:26 +0300

    Классы, объекты, наследование. 1. Прямоугольник(1)

--- Klasses_obj_inherit.py
+++ Klasses_obj_inherit.py
@@ -9,23 +9,13 @@ class Rectangle:
     def draw(self):
         for i in range(1, self.width + 1):
             for n in range(1, self.length + 1):
-                print('*',end='')
+                print('*', end='')
             print(end='\n')
 
-# r1 = Rectangle()
-# r2 = Rectangle(5, 3)
-# print(r1.calculate_area())
-# print(r2.calculate_area())
-# r1.draw()
-# r2.draw()
+r1 = Rectangle()
+r2 = Rectangle(5, 3)
+print(r1.calculate_area())
+print(r2.calculate_area())
+r1.draw()
+r2.draw()
 
-class Square(Rectangle):
-    def __init__(self, length=3):
-        super().__init__(length, length)
-
-s1 = Square()
-s1.draw()
-print(s1.calculate_area())
-s2 = Square(6)
-s2.draw()
-print(s2.calculate_area())
