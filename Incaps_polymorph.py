828507580192b240b773c0b1007db3d12bbc933a
4MAxx <mad_@tut.by>
2022-05-17 00:06:12 +0300

    Инкапсуляция и полиморфизм. 2. Робот

--- Incaps_polymorph.py
+++ Incaps_polymorph.py
@@ -12,9 +12,30 @@ class UserMail:
         else: print('Ошибочная почта')
 
 
-k = UserMail('belosnezhka227', 'prince@wait.you')
+# k = UserMail('belosnezhka227', 'prince@wait.you')
+# # print(k.get_email())
+# k.set_email([1, 2, 3])
+# k.set_email('prince@wait@.you')
+# k.set_email('fafa@mail.ru')
 # print(k.get_email())
-k.set_email([1, 2, 3])
-k.set_email('prince@wait@.you')
-k.set_email('fafa@mail.ru')
-print(k.get_email())
+
+class Robot:
+
+    def __init__(self, name, year, psyh, fiz):
+        self.name = name
+        self.year = year
+        self.__psyh = psyh
+        self.__fiz = fiz
+        self.__condition = psyh + fiz
+
+    def get_condition(self):
+        if self.__condition <= -1: return 'Я чувствую себя несчастным!'
+        elif -1 < self.__condition <= 0: return 'Я чувствую себя плохо!'
+        elif 0 < self.__condition <= 0.5: return 'Могло быть хуже!'
+        elif 0.5 < self.__condition <= 1: return 'Кажется все в порядке!'
+        else: return 'Супер!'
+
+robot1 = Robot('Marvin', 1979, 0.2, 0.4)
+robot2 = Robot('Caliban', 1993, -0.4, 0.3)
+print(robot1.get_condition())
+print(robot2.get_condition())
